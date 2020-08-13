6c4d15f6e5f949644dba846d98b02cc2830b78e9
matiascardozo <matiasicardozol@gmail.com>
2020-08-12 22:21:46 -0400

    Add endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function (req, res, next) {
+    const { body } = req;
+    const response = { ...body, status: 200 };
+    return res.send(response);
 });
 
 module.exports = router;
