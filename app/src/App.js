e39014247c60b274aeb53b7cd83ccb95e20b278c
matiascardozo <matiasicardozol@gmail.com>
2020-08-13 23:04:13 -0400

    React app starter

---
+++ app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="container d-flex w-100 h-100 p-3 mx-auto flex-column align-center App">
+            <header className="masthead mb-auto">
+               
+            </header>
+            <main role="main" className="inner cover">
+                <h1 className="cover-heading">Cover your page.</h1>
+                <p className="lead">
+                    Cover is a one-page template for building simple and beautiful home pages.
+                    Download, edit the text, and add your own fullscreen background photo to make it
+                    your own.
+                </p>
+                <p className="lead">
+                    <a href="#" className="btn btn-lg btn-secondary">
+                        Learn more
+                    </a>
+                </p>
+            </main>
+            <footer className="mastfoot mt-auto">
+                
+            </footer>
+        </div>
+    );
+}
+
+export default App;
