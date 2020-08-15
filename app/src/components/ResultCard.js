7ebba49cbc474e92fa597ebe0f7dc14de99302c9
matiascardozo <matiasicardozol@gmail.com>
2020-08-15 12:11:07 -0400

    Fix class prop name

--- app/src/components/ResultCard.js
+++ app/src/components/ResultCard.js
@@ -4,11 +4,11 @@ import React from 'react';
 
 function ResultCard({ result, clearElement }) {
     return (
-        <div class={`card ${result.statusCode ? 'border-danger' : 'border-success'} mb-2 border-3`}>
-            <div class="card-body text-dark">
-                <div class="row no-gutters">
-                    <div class="col"><span>{result.message}</span></div>
-                    <div class="col-sm-1">
+        <div className={`card ${result.statusCode ? 'border-danger' : 'border-success'} mb-2 border-3`}>
+            <div className="card-body text-dark">
+                <div className="row no-gutters">
+                    <div className="col"><span>{result.message}</span></div>
+                    <div className="col-sm-1">
                         <FontAwesomeIcon onClick={clearElement} className="cursor-pointer" icon={faTrash} />
                     </div>
                 </div>
